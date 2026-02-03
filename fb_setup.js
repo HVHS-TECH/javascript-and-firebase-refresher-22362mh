7a9e394b997f395984af6b04ca28da4c1d81f1d9
Mio Hoffman <22362mh@hvhs.school.nz>
2026-02-04 12:24:21 +1300

    Got javascript connected, started on firebase

---
+++ fb_setup.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDbYQyRprjQJLj_2AHnmsySQA30YWAMo7o",
+  authDomain: "miohoffman-13comp.firebaseapp.com",
+  databaseURL: "https://miohoffman-13comp-default-rtdb.firebaseio.com",
+  projectId: "miohoffman-13comp",
+  storageBucket: "miohoffman-13comp.firebasestorage.app",
+  messagingSenderId: "588356098707",
+  appId: "1:588356098707:web:5bd99a74caeae5c6388b66",
+  measurementId: "G-YMVSCX6JL9"
+};
--- script.js
+++ script.js
@@ -1,25 +1,16 @@
-var messageSpace = getElementbyId("welcomeMessage");
-messageSpace.innerHTML = "You've connected to the JavaScript!";
+var messageSpace = document.getElementById("welcomeMessage");
+messageSpace.innerHTML = "<h1>You've connected to the JavaScript!<h1>";
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+function getFormInput(){
+  const NAME = document.getElementById("input");
+  let userName = NAME.value;
+  messageSpace.innerHTML = "<h1> Your name is " + userName + " </p>"
+}
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDbYQyRprjQJLj_2AHnmsySQA30YWAMo7o",
-  authDomain: "miohoffman-13comp.firebaseapp.com",
-  databaseURL: "https://miohoffman-13comp-default-rtdb.firebaseio.com",
-  projectId: "miohoffman-13comp",
-  storageBucket: "miohoffman-13comp.firebasestorage.app",
-  messagingSenderId: "588356098707",
-  appId: "1:588356098707:web:5bd99a74caeae5c6388b66",
-  measurementId: "G-YMVSCX6JL9"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+function firebaseInput(){
+  firebaseInput.database().ref('/').set(
+    {
+      messsage: 'Hello'
+    }
+  )
+}
